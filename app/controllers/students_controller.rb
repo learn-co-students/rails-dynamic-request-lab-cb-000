7ff2648912e17a66cda2ee6748629eeb819237db
Cesar Castillo <cesarcast1llo@yahoo.com>
2017-11-04 02:57:18 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,11 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = Student.find(params[:id])
+  end
 end
+
+#index still shows all students
+#index is picking out specific student by id
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
+  resources :students, only: [:index, :show]
 end
+#before it was specifically to index, now its no any student call out
