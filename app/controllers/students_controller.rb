984197bdf3c39a8787835a5d3b45d87efc9a2ec4
Alexander Spanos <alexisspa9@gmail.com>
2018-04-03 17:12:18 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,7 +3,7 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
   def show
-    @student = Student.find_by_id(params[:id])
+    @student = Student.find(params[:id])
     
   end
 end
